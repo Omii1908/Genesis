dd726c38e9fb60426b4ca936d87c80bb8612ecbb
Omii1908 <Omi844198@gmail.com>
2025-08-03 19:19:53 +0530

    Creating dynamic URLs

--- web/django/gettingStarted/gettingStarted/views.py
+++ web/django/gettingStarted/gettingStarted/views.py
@@ -5,3 +5,9 @@ def aboutus(request):
 
 def home(request):
     return HttpResponse("<b>This is Home page..</b>")
+
+def course(request):
+    return HttpResponse("<b>This is Course page..</b>")
+
+def course_detail(request, course_id):
+    return HttpResponse(f"<b>This is Course Detail page for course {course_id}..</b>")
